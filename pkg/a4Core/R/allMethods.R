60a07003689a76106e8476d5f9c0ca49b09c63f9
tobias_verbeke (Tobias Verbeke) <tobias.verbeke@telenet.be>
2010-02-21 09:10:55 +0000

    a4Core, version 0.0-1
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/a4@29 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/a4Core/R/allMethods.R
+++ pkg/a4Core/R/allMethods.R
@@ -1,8 +1,9 @@
-confusionMatrix <- function(x, ...){
+confusionMatrix <- function(x, ...){ # common to a4Classif (pamClass) and nlcv
   UseMethod("confusionMatrix")
 }
 
-setGeneric("topTable", function(fit, n, ...){
-      standardGeneric("topTable")
+setGeneric("topTable", function(fit, n, ...){ # common to nlcv and (at least) a4Classif
+  standardGeneric("topTable")
 })
+
 
