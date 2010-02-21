4a4248e3a9b7da59ae21cc378ac1ed12ee8baf5c
tobias_verbeke (Tobias Verbeke) <tobias.verbeke@telenet.be>
2010-02-21 10:25:40 +0000

    xtable methods split off a4Classif
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/a4@34 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/a4Reporting/R/xtable.methods.R
@@ -0,0 +1,19 @@
+xtable.topTableGlmnet <- function(x, caption = NULL, label = NULL, align = NULL, 
+    digits = NULL, display = NULL, ...){
+  
+  xtable:::xtable.data.frame(x$topList, caption = caption, label = label, align = align,
+      digits = digits, display = display, ...)
+}
+
+xtable.pamClassConfusionTable <- function(x, caption = NULL, label = NULL, align = NULL, 
+    digits = NULL, display = NULL, ...){
+  
+  # TODO: some preprocessing
+  xtable:::xtable.matrix(x, caption = caption, label = label, align = align,
+      digits = digits, display = display, ...)
+}
+
+xtable.topTablePam <- function(x, ...){
+  xtable(x$topList, ...)
+}
+
--- pkg/a4Reporting/R/xtable.topTable.R
+++
@@ -1,7 +0,0 @@
-xtable.topTableGlmnet <- function(x, caption = NULL, label = NULL, align = NULL, 
-    digits = NULL, display = NULL, ...){
-  
-  xtable:::xtable.data.frame(x$topList, caption = caption, label = label, align = align,
-      digits = digits, display = display, ...)
-}
-
